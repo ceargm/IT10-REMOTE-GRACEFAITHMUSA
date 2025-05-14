d85734aa570a36fcae41116b2c062e61e54d50c0
ceargm <gracefaithmusa1022@gmail.com>
2025-05-14 17:04:08 +0800

    Implemented font size change on selection

--- Desktop/IT10-LOCAL-GRACEFAITHMUSA/script.js
+++ Desktop/IT10-LOCAL-GRACEFAITHMUSA/script.js
@@ -1,7 +1,15 @@
 let fontSelect = document.getElementById("fontSelect");
 let displayText = document.getElementById("displayText");
 
+let sizeSelect = document.getElementById("sizeSelect");
+
 fontSelect.addEventListener("click", function() {
     const selectedFont = fontSelect.value;
     displayText.style.fontFamily = selectedFont;
+});
+
+
+sizeSelect.addEventListener("click", function() {
+    const selectedSize = sizeSelect.value;
+    displayText.style.fontSize = selectedSize;
 });
