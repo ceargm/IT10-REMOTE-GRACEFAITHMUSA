683cb2fe3880f20b45733e489a840b509fe342db
ceargm <gracefaithmusa1022@gmail.com>
2025-05-14 17:06:25 +0800

    Implemented font color change on selection

--- Desktop/IT10-LOCAL-GRACEFAITHMUSA/script.js
+++ Desktop/IT10-LOCAL-GRACEFAITHMUSA/script.js
@@ -2,6 +2,7 @@ let fontSelect = document.getElementById("fontSelect");
 let displayText = document.getElementById("displayText");
 
 let sizeSelect = document.getElementById("sizeSelect");
+let colorSelect = document.getElementById("colorSelect");
 
 fontSelect.addEventListener("click", function() {
     const selectedFont = fontSelect.value;
@@ -12,4 +13,9 @@ fontSelect.addEventListener("click", function() {
 sizeSelect.addEventListener("click", function() {
     const selectedSize = sizeSelect.value;
     displayText.style.fontSize = selectedSize;
+});
+
+colorSelect.addEventListener("click", function() {
+    const selectedColor = colorSelect.value;
+    displayText.style.color = selectedColor;
 });
